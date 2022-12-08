a2096ec6bb4d431005dab930ed74c4112113d167
JRToyoda <therubikators@gmail.com>
2022-12-08 21:51:42 +0800

    version 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ side_one = (input("Enter the first side: "))
 side_two = (input("Enter the second side: "))
 side_three = (input("Enter the third side: "))
 
-while side_one == side_two == side_three == "":
+while (side_one != "") & (side_two != "") & (side_three != ""):
 
     if side_one == side_two == side_three:
         print("The triangle is equilateral.\n ")
