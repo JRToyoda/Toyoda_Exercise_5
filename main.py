5785c3d2a417a6f0cf49a89a3d6cbf2efbd64ed7
JRToyoda <therubikators@gmail.com>
2022-12-09 18:29:26 +0800

    version 2

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-while True:
+side_one = (input("Enter the first side: "))
+side_two = (input("Enter the second side: "))
+side_three = (input("Enter the third side: "))
 
-    side_one = (input("Enter the first side: "))
-    side_two = (input("Enter the second side: "))
-    side_three = (input("Enter the third side: "))
+if side_one == side_two == side_three:
+    print("The triangle is equilateral.\n ")
 
-    if side_one == side_two == side_three:
-        print("The triangle is equilateral.\n ")
+elif (side_one != "") & (side_two != "") & (side_three != ""):
+    print("The triangle is not equilateral.\n ")
 
-    elif (side_one != "") & (side_two != "") & (side_three != ""):
-        print("The triangle is not equilateral.\n ")
-
-    else:
-        print("Error\n")
+else:
+    print("Error\n")
 
 
 
