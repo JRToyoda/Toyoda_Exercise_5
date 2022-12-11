c53fda16ef5d3ca0bb5f3a5dee04995d14f09815
JRToyoda <therubikators@gmail.com>
2022-12-11 18:04:30 +0800

    version 2

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ side_one = (input("Enter the first side: "))
 side_two = (input("Enter the second side: "))
 side_three = (input("Enter the third side: "))
 
-if side_one == side_two == side_three:
+if not side_one.isnumeric() or not side_two.isnumeric() or not side_three.isnumeric():
+    print("Please input positive integer\n")
+
+elif side_one == side_two == side_three:
     print("The triangle is equilateral.\n ")
 
-elif (side_one != "") & (side_two != "") & (side_three != ""):
+elif side_one != "" or side_two != "" or side_three != "":
     print("The triangle is not equilateral.\n ")
-
-else:
-    print("Error\n")
-
-
-
